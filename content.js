40a3fd5aea94d537bcfa1eb665089ce22d371881
Saumya Garg <saumyagarg.2409@gmail.com>
2025-06-10 05:00:55 +0530

    Delete content.js

--- content.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('mouseup', function () {  
-    const selectedText = window.getSelection().toString().trim();  
-    if (selectedText.length > 0) {  
-        chrome.runtime.sendMessage({ action: 'highlightedText', text: selectedText });  
-    }  
-});  
